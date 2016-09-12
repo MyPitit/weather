b6bc192af3088737de17ba375342f8e939ddc4c5
Mireia <mireia@mireiasangalo.com>
2016-09-12 14:59:12 +0100

    removing comments

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,13 +1,5 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//     render: function () {
-//         return (
-//             <h3>Examples component</h3>
-//         );
-//     }
-// });
-
 var Examples = (props) => {
      return (
         <di>
